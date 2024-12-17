55439d5668a071edef7abc841959fe9f3352591b
Tomasz Trzciński <trzcinski.tomasz.1988@gmail.com>
2024-12-17 18:38:15 +0100

    Created Unit Test to check rolling and reading from list those rolls.

--- myproject/myapp/tests.py
+++ myproject/myapp/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+from .models import SingleRoll
 
-# Create your tests here.
+class SingleRollAPITestCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        SingleRoll.objects.create(name="1")
+        SingleRoll.objects.create(name="20")
+
+    def test_get_rolls(self):
+        response = self.client.get(reverse('rolls-list'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # There will be 3, because 2 stubbed and 1 random
+        self.assertEqual(len(response.data), 3)
